179293b53ca80b467f2ab83045a414c0ebf6fa20
Matt Godfrey <mcgodfrey@users.noreply.github.com>
2015-12-13 14:47:29 +1100

    Switched to 20x4 lcd (instead of 16x2) Changed the poinout slightly (which analog input pins were used) It is now tested and installed in the power supply and working.

--- lab_supply_monitor.ino
+++ lab_supply_monitor.ino
@@ -10,7 +10,7 @@
  * Rs = 7
  * Enable = 8
  * D4 = 9
- * D5 = 19
+ * D5 = 10
  * D6 = 11
  * D7 = 12
  * RW to gnd
@@ -20,17 +20,18 @@
 
 #include <LiquidCrystal.h>
 
-int Vset = A0;
-int Vmeas = A1;
-int Iset = A2;
-int Imeas = A3;
+int Vset = A7;
+int Iset = A6;
+int Vmeas = A5;
+int Imeas = A4;
 
+//               (RS, EN, D4, D5, D6, D7)
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 
 void setup() {
   //lcd setup
-  lcd.begin(16, 2);
+  lcd.begin(20, 4);
   lcd.clear();
   
   //ADC pin setup
@@ -44,29 +45,33 @@ void setup() {
 
 void loop() {
   //meaure values
-  float set_voltage = (57.0/10.0)*5.0/1024.0*analogRead(Vset);
-  float meas_voltage = (57.0/10.0)*5.0/1024.0*analogRead(Vmeas);
-  float set_current = analogRead(Iset)*(5.0/1024.0)/(0.004*0.2*2200.0);
-  float meas_current = analogRead(Imeas)*(5.0/1024.0)/(0.004*0.2*2200.0);
+  float set_voltage = 5.64*5.0/1024.0*analogRead(Vset);
+  //float set_voltage = 5.0/1024.0*analogRead(Vset);
+  float meas_voltage = 5.64*5.0/1024.0*analogRead(Vmeas);
+  //float meas_voltage = 5.0/1024.0*analogRead(Vmeas);
+  //float set_current = analogRead(Iset)*(5.0/1024.0)/(0.004*0.2*2200.0);
+  float set_current = analogRead(Iset)*(5.0/1024.0);
+  //float meas_current = analogRead(Imeas)*(5.0/1024.0)/(0.004*0.2*2200.0); 
+  float meas_current = analogRead(Imeas)*(5.0/1024.0);
 
   //update the lcd
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("V = ");
-  lcd.setCursor(4, 0);
+  lcd.print("Vmeas = ");
+  lcd.setCursor(8, 0);
   lcd.print(meas_voltage);
-  lcd.setCursor(9,0);
-  lcd.print("(");
-  lcd.print(set_voltage);
-  lcd.print(")");
   lcd.setCursor(0, 1);
-  lcd.print("I = ");
-  lcd.setCursor(4, 1);
-  lcd.print(meas_current,3);
-  lcd.setCursor(9,1);
-  lcd.print("(");
-  lcd.print(set_current,3);
-  lcd.print(")");
-
-  delay(100);  
+  lcd.print("Vset = ");
+  lcd.setCursor(8, 1);
+  lcd.print(set_voltage);
+  lcd.setCursor(0, 2);
+  lcd.print("Imeas = ");
+  lcd.setCursor(8, 2);
+  lcd.print(meas_current);
+  lcd.setCursor(0, 3);
+  lcd.print("Iset = ");
+  lcd.setCursor(8, 3);
+  lcd.print(set_current);
+  
+  delay(50);  
 }
